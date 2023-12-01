f04a7a4ccc61c617e65aa9ac9c66af126480b377
vboxuser <kristjan.toodo@gmail.com>
2023-12-01 16:10:17 +0200

    Removed imports

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,6 @@ package org.example;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
